ba74f80f040c3c3ea15131a75d325680ec264882
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2025-07-06 14:48:29 +0300

    Rename phpdoc to boson

--- Makefile
+++ Makefile
@@ -6,19 +6,19 @@ down:
 	docker compose down
 
 php:
-	docker exec -it phpdoc-php bash
+	docker exec -it boson-php bash
 
 php-root:
-	docker exec -it -uroot phpdoc-php bash
+	docker exec -it -uroot boson-php bash
 
 node:
-	docker exec -it -u1000 phpdoc-node sh
+	docker exec -it -u1000 boson-node sh
 
 angie:
-	docker exec -it phpdoc-angie sh
+	docker exec -it boson-angie sh
 
 angie-root:
-	docker exec -it -uroot phpdoc-angie sh
+	docker exec -it -uroot boson-angie sh
 
 deploy: build push
 
--- docker/php/entrypoint.dev.sh
+++ docker/php/entrypoint.dev.sh
@@ -5,14 +5,14 @@ set -e
 #   Generate Dev Keys
 # ------------------------------------------------------------------------------
 
-if [ ! -f /home/phpdoc/phpdoc.app/config/secrets/dev/auth.key ]; then
-  cp -R /home/phpdoc/phpdoc.app/config/secrets/test/auth.key \
-        /home/phpdoc/phpdoc.app/config/secrets/dev/auth.key
+if [ ! -f /home/boson/bosonphp.com/config/secrets/dev/auth.key ]; then
+  cp -R /home/boson/bosonphp.com/config/secrets/test/auth.key \
+        /home/boson/bosonphp.com/config/secrets/dev/auth.key
 fi
 
-if [ ! -f /home/phpdoc/phpdoc.app/config/secrets/dev/auth.key.pub ]; then
-  cp -R /home/phpdoc/phpdoc.app/config/secrets/test/auth.key.pub \
-        /home/phpdoc/phpdoc.app/config/secrets/dev/auth.key.pub
+if [ ! -f /home/boson/bosonphp.com/config/secrets/dev/auth.key.pub ]; then
+  cp -R /home/boson/bosonphp.com/config/secrets/test/auth.key.pub \
+        /home/boson/bosonphp.com/config/secrets/dev/auth.key.pub
 fi
 
 source /usr/local/bin/entrypoint.sh;
