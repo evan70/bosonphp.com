33fb9d2d94e6c57fc91235b148afd9ed07daaf3d
SerafimArts <nesk@xakep.ru>
2025-07-10 01:45:09 +0300

    Fix and simplify PageTypeOutput enum

--- app/Documentation/Application/Output/Page/PageTypeOutput.php
+++ app/Documentation/Application/Output/Page/PageTypeOutput.php
@@ -4,20 +4,8 @@ declare(strict_types=1);
 
 namespace App\Documentation\Application\Output\Page;
 
-use App\Documentation\Domain\PageType;
-
 enum PageTypeOutput
 {
     case Document;
     case Link;
-    case Unknown;
-
-    public static function fromPageType(PageType $type): self
-    {
-        return match ($type) {
-            PageType::Document => self::Document,
-            PageType::Link => self::Link,
-            default => self::Unknown,
-        };
-    }
 }
