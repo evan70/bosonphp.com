00eab5009faa20384cba92d4e5d77f959321a389
SerafimArts <nesk@xakep.ru>
2025-07-13 21:31:31 +0300

    Move docs renderer to Renderer namespace

--- app/Documentation/Infrastructure/Renderer/PageDocumentContentRenderer.php
+++ app/Documentation/Infrastructure/Renderer/PageDocumentContentRenderer.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Documentation\Infrastructure\Content;
+namespace App\Documentation\Infrastructure\Renderer;
 
 use App\Documentation\Domain\PageDocumentContent;
 use App\Documentation\Domain\PageDocumentContentRendererInterface;
@@ -12,7 +12,7 @@ use League\CommonMark\ConverterInterface;
  * @api
  *
  * @internal this is an internal library class, please do not use it in your code
- * @psalm-internal App\Documentation\Infrastructure\Content
+ * @psalm-internal App\Documentation\Infrastructure\Renderer
  */
 final readonly class PageDocumentContentRenderer implements PageDocumentContentRendererInterface
 {
