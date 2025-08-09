8596ab63304f761b80b6c09c6b4854f623ce09f9
SerafimArts <nesk@xakep.ru>
2025-08-10 01:54:53 +0300

    Improve tooltips

---
+++ public/build/assets/stateDiagram-v2-DCU4277D-BxiY1Eze.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-NOLL7BTH--8RQVM97.js";import{_ as i}from"./app-C9enxA37.js";import"./chunk-FOZUH6DQ-8SprH0tc.js";import"./chunk-RKBYWBGH-C0K6AUSu.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
