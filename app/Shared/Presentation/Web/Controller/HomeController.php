aeae45350b439a04bf80ee056b7d4dc0004c4362
SerafimArts <nesk@xakep.ru>
2025-08-17 15:04:32 +0300

    Close landing

--- app/Shared/Presentation/Web/Controller/HomeController.php
+++ app/Shared/Presentation/Web/Controller/HomeController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Shared\Presentation\Web\Controller;
 
-use App\Documentation\Domain\Version\Repository\CurrentVersionProviderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,17 +11,8 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/', name: 'home', methods: 'GET')]
 final class HomeController extends AbstractController
 {
-    public function __construct(
-        private readonly CurrentVersionProviderInterface $versions,
-    ) {}
-
     public function __invoke(): Response
     {
-        $version = $this->versions->findLatest();
-
-        return $this->render('page/home.html.twig', [
-            'version' => $version,
-            'categories' => $version->categories ?? [],
-        ]);
+        return $this->redirectToRoute('doc.index', status: Response::HTTP_TEMPORARY_REDIRECT);
     }
 }
