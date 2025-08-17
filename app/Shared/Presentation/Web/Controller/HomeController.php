2950470f4746a6d55bae36610a55fdd1e1985da2
SerafimArts <nesk@xakep.ru>
2025-08-17 14:57:47 +0300

    Revert back landing

--- app/Shared/Presentation/Web/Controller/HomeController.php
+++ app/Shared/Presentation/Web/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Presentation\Web\Controller;
 
+use App\Documentation\Domain\Version\Repository\CurrentVersionProviderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,17 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/', name: 'home', methods: 'GET')]
 final class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly CurrentVersionProviderInterface $versions,
+    ) {}
+
     public function __invoke(): Response
     {
-        return $this->redirectToRoute('doc.index', status: Response::HTTP_TEMPORARY_REDIRECT);
+        $version = $this->versions->findLatest();
+
+        return $this->render('page/home.html.twig', [
+            'version' => $version,
+            'categories' => $version->categories ?? [],
+        ]);
     }
 }
